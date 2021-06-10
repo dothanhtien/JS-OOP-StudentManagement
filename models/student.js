ae050a26aa08a067c708aad3ac7f729ab38a58b8
Tien Do Thanh <dothanhtien95@gmail.com>
2021-06-10 23:49:45 +0700

    Create Student object

---
+++ models/student.js
@@ -0,0 +1,8 @@
+function Student(id, name, gender, math, physics, chemistry) {
+  this.id = id;
+  this.name = name;
+  this.gender = gender;
+  this.math = math;
+  this.physics = physics;
+  this.chemistry = chemistry;
+}
