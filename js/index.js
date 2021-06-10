4163f9358067f1ab6bc2f350ac30974eff330e0f
Tien Do Thanh <dothanhtien95@gmail.com>
2021-06-11 00:13:59 +0700

    Add addStudent method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 var studentList = [];
+
+var addStudent = function () {
+  var id = document.querySelector("#txtId").value;
+  var name = document.querySelector("#txtName").value;
+  var gender = getGender();
+  var math = +document.querySelector("#txtMath").value;
+  var physics = +document.querySelector("#txtPhysics").value;
+  var chemistry = +document.querySelector("#txtChemistry").value;
+
+  var newStudent = new Student(id, name, gender, math, physics, chemistry);
+  studentList.push(newStudent);
+  console.log(studentList);
+};
+
+var getGender = function () {
+  var genderRadios = document.getElementsByName("genderRadios");
+
+  for (var i = 0; i < genderRadios.length; i++) {
+    if (genderRadios[i].checked) {
+      return genderRadios[i].value;
+    }
+  }
+
+  return "";
+};
